32f79cc86d5193de988d5da135991644cefb6e77
sfujiwara <shuhei.fujiwara@gmail.com>
2022-07-20 05:02:47 +0900

    invoke tasks for docker

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,3 +1,4 @@
+import os
 import invoke
 import toml
 
@@ -10,3 +11,30 @@ def get_version() -> str:
         pyproject = toml.load(f)
 
     return pyproject["tool"]["poetry"]["version"]
+
+
+@invoke.task
+def docker_build(c):
+    """
+    Build Docker image.
+    """
+    os.environ["VERSION"] = get_version()
+    c.run("docker-compose build")
+
+
+@invoke.task
+def docker_push(c):
+    """
+    Build and push Docker image.
+    """
+    os.environ["VERSION"] = get_version()
+    c.run("docker-compose build")
+    c.run("docker-compose push")
+
+
+d = invoke.Collection("docker")
+d.add_task(task=docker_build, name="build")
+d.add_task(task=docker_push, name="push")
+
+ns = invoke.Collection()
+ns.add_collection(d)
